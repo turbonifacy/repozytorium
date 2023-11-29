0df42408e5c9a68085ce97e6dbde234bc90a0fd1
bonifacy <146916000+turbonifacy@users.noreply.github.com>
2023-11-29 08:13:11 +0100

    test
    ok

---
+++ untitled/src/main/java/workshop/Tool.java
@@ -0,0 +1,11 @@
+package workshop;
+public class Tool{
+    String toolType;
+    public Tool() {
+        this.toolType = toolType;
+    }
+    public void useTool() {
+        System.out.println("narzedize: " + toolType);
+    }
+}
+
