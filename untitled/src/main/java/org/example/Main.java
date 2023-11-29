8c59bf350da90af5c7754c8bbe4e4b2c2de7fd87
bonifacy <146916000+turbonifacy@users.noreply.github.com>
2023-11-29 08:27:47 +0100

    Delete untitled directory

--- untitled/src/main/java/org/example/Main.java
+++
@@ -1,7 +0,0 @@
-package workshop;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- untitled/src/src/main/java/workshop/Boss.java
+++
@@ -1,9 +0,0 @@
-package workshop;
-
-public class Boss {
-
-    public static void main(String[] args) {
-        Worker andrzej = new Worker();
-        andrzej.run();
-    }
-}
